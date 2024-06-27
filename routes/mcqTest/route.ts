4ad9445b9e29d4758d96f71eb5e4bb56ff36fdad
Andev70 <anupalnath70@gmail.com>
2024-06-27 16:35:36 +0530

    answer submission

--- routes/mcqTest/route.ts
+++ routes/mcqTest/route.ts
@@ -4,6 +4,7 @@ import { GetAllTests } from "../../controller/getTests";
 import { getOneQuestion } from "../../controller/getQuestion";
 import { getTestResult } from "../../controller/getResult";
 import { pushResult } from "../../controller/pushResult";
+import { skipQuestion } from "../../controller/skipQuestion";
 const mcqTestRouter = express.Router();
 
 mcqTestRouter.route("/tests/unique").get(GetAllTests);
@@ -13,4 +14,5 @@ mcqTestRouter
 mcqTestRouter.route("/create/result").post(createResult);
 mcqTestRouter.route("/get/result").post(getTestResult);
 mcqTestRouter.route("/push/answer").post(pushResult);
+mcqTestRouter.route("/skip/answer").post(skipQuestion);
 export { mcqTestRouter };
